a6c968c948d9969cceba3e4f9397d04bd91b7878
TheCsarbeat <62681287+TheCsarbeat@users.noreply.github.com>
2021-04-23 14:03:14 -0600

    Menú
    La unión de todas las funciones y validaciones, funcional y abierta a cambios.

--- funciones.py
+++ funciones.py
@@ -2,7 +2,7 @@
 #Fecha de realización:14/04/2021 08:39 p.m.
 #Última modificación:14/04/2021 03:39  p.m.
 #Versión: 3.8.2
-
+from datetime import datetime
 def cifrarMurcielago(texto):
     """
     Funcionamiento: cifra el texto al código murciélago usado por los scouts
@@ -10,7 +10,6 @@ def cifrarMurcielago(texto):
     Salidas: retorna el texto cifrado en código murciélago
     """
     impresion = ""
-    texto = texto.lower()
     code = ["m","u","r","c","i","e","l","a","g","o"]
     for i in texto:
         if i in code:
@@ -20,7 +19,6 @@ def cifrarMurcielago(texto):
         else:
             impresion+=i
     return impresion
-
 def descifrarMurcielago(texto):
     """
     Funcionamiento: descifra el texto en código muerciélago a su equivalente español
@@ -38,7 +36,6 @@ def descifrarMurcielago(texto):
         else:
             impresion+=i
     return impresion
-
 def cifrarCenitPolar(texto):
     """
     Funcionamiento: cifra el texto al cenit polar usado por los scouts, así como descifrarlo
@@ -65,7 +62,6 @@ def cifrarCenitPolar(texto):
         else:
             impresion+=i
     return impresion
-
 def cifrarDeletreo(texto):
     """
     Funcionamiento: cifra el texto en código deletreo
@@ -80,10 +76,9 @@ def cifrarDeletreo(texto):
         for j in code:
             if i == j[0]:
                 impresion+=j
-                if not i == texto[-1]: #esto es para que cuando llegue a la ultima posicion no vuelva a agregar ~
-                    impresion+="~"
+                impresion+="~"
+    impresion = limpiarUltimoCaracter(impresion)
     return impresion
-
 def descifrarDeletreo(texto):
     """
     Funcionamiento: descifra el texto del código deletreo al español
@@ -93,5 +88,191 @@ def descifrarDeletreo(texto):
     impresion = ""
     code = texto.lower().split("~")
     for i in code:
-        impresion+=i[0]
+        if not i == "":
+            impresion+=i[0]
     return impresion
+def cifrarEucalipto(texto):
+    """
+    Funcionamiento: Encripta el texto ingresado en código eucalipto
+    Entradas:
+    -texto(string): El texto a encriptar
+    Salidas: 
+    -resultado(string): El texto encriptado
+    """
+    eucalipto = ["e","u","c","a","l","i","p","t","o"]
+    texto = texto.lower()
+    resultado=""
+    for i in texto:
+        if i in eucalipto:
+            resultado+= str(eucalipto.index(i)+1)
+        elif i == " ":
+            resultado+= "°"
+        else:
+            resultado+= i
+    return resultado
+
+def descifrarEucalipto(texto):
+    """
+    Funcionamiento: Desencripta el texto ingresado en código eucalipto
+    Entradas:
+    -texto(string): El texto a desencriptar
+    Salidas: 
+    -resultado(string): El texto desencriptar
+    """
+    eucalipto = ["e","u","c","a","l","i","p","t","o"]
+    texto = texto.lower()
+    resultado=""
+    for i in texto:
+        if i.isdigit():
+            resultado+= eucalipto[int(i)-1]
+        elif i == "°":
+            resultado+= " "
+        else:
+            resultado+= i    
+    return resultado
+
+def cifrarMorse(texto):
+    """
+    Funcionamiento: Encripta el texto ingresado en código morse
+    Entradas:
+    -texto(string): El texto a encriptar
+    Salidas: 
+    -resultado(string): El texto encriptado
+    """
+    morse = [".-", "-...", "-.-.","-..", ".","..-.","--.","....", "..","·---","-.-",".-..","--", "-.", "--.--", "---",".--.","--.-",".-.", "...",\
+    "-","..-","...-",".--","-..-","-.--","--..",  "-----", ".----", "..---", "...--", "....-",".....", "-....", "--...", "---..", "----.",]
+    alfabeto = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9"]
+    texto = texto.lower()
+    resultado=""
+    for i in texto:
+        if i in alfabeto:
+            resultado+= str(morse[alfabeto.index(i)])+"^"
+        else:
+            resultado = limpiarUltimoCaracter(resultado)
+            resultado+= "|"
+    resultado = limpiarUltimoCaracter(resultado)
+    return resultado
+
+def descifrarMorse(texto):
+    """
+    Funcionamiento: Encripta el texto ingresado en código morse
+    Entradas:
+    -texto(string): El texto a encriptar
+    Salidas: 
+    -resultado(string): El texto encriptado
+    """
+    morse = [".-", "-...", "-.-.","-..", ".","..-.","--.","....", "..",".---","-.-",".-..","--", "-.","---",".--.","--.-",".-.", "...",\
+    "-","..-","...-",".--","-..-","-.--","--..",  "-----", ".----", "..---", "...--", "....-",".....", "-....", "--...", "---..", "----.",]
+    alfabeto = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9"]
+    texto = texto.lower()
+    resultado=""
+    caracteres = texto.split("^")
+    for i in caracteres:
+        if i in morse:
+            resultado+= str(alfabeto[morse.index(i)])
+        elif "|" in i: 
+            for j in i.split("|"):
+                if j in morse :
+                    resultado+= str(alfabeto[morse.index(j)])+" "
+                elif j != "": # no sirve hacer un else solo ya que lo que se queire es olo no esta en los códigos morse que imprima un caracter in vlálido
+                    resultado += "?"
+            resultado = limpiarUltimoCaracter(resultado)
+        elif i != "": # no sirve hacer un else solo ya que lo que se queire es olo no esta en los códigos morse que imprima un caracter in vlálido
+            resultado += "?"
+    return resultado
+
+def determinarPar(num):
+    """
+    Funcionamiento: Determina si el número es par
+    Entradas:
+    -num(int): número a válidar
+    Salidas: 
+    -True: si es par
+    -False: si no es par
+    """
+    if num % 2 ==0:
+        return True
+    else:
+        return False
+
+def codigoSufamelico(texto):
+    """
+    Funcionamiento: cifra el texto al sufamelico usado por los scouts, así como descifrarlo
+    la razón por la que NO se dividen las tareas, es porque este código y la manera en que está
+    programado permite que se le ingrese un texto cifrado y lo descifre sin necesidad de hacer
+    ningún cambio mayor que implicase rigidez en el código
+    Entradas: 
+    -texto: el texto a cifrar del usuario
+    Salidas:
+    -retorna el texto cifrado en código cenit polar
+    """
+    impresion = ""
+    texto = texto.lower()
+    code = ["s","u","f","a","m","e","l","i","c","o"]
+    for i in texto:
+        if i in code:
+            if determinarPar(code.index(i)):
+                impresion+=code[code.index(i)+1]
+            else:
+                impresion+=code[code.index(i)-1]
+        else:
+            impresion+=i
+    return impresion
+
+def limpiarUltimoCaracter(texto):
+    """
+    Funcionamiento: Se encarga de eliminar el último caracter de una cadena de texto
+    Entradas: 
+    -texto(string): la variable a utilizar
+    Salidas:
+    -retorna el texto sin el último caracter
+    """
+    return texto[:len(texto)-1]
+
+def buscarBitacora():
+    """
+    Funcionamiento: Determina si el archiv de bitácora existe.
+    Entradas: 
+    -NA
+    Salidas:
+    -True: si el archivo existe
+    -False: si no existe
+    """       
+    try:
+        bitacora = open("Bitácora.txt","r")
+        bitacora.close()
+        return True
+    except:
+        return False
+        
+def añadirBitacora(proceso,entrada,salida):
+    """
+    Funcionamiento: Agrega al archivo bitacora un nuevo registro.
+    Entradas: 
+    -proceso(string): El nombre del proceso que se ejecutó
+    -entrada(string): La entrada que recibió el procesos ejecutado.
+    -salida(string): La respuesta del proceso ejecutado.
+    Salidas:
+    -Una nueva línea en el archivo de bitácora.
+    """       
+    now = datetime.now()
+    if buscarBitacora():
+        bitacora = open("Bitácora.txt","a")
+        bitacora.write(now.strftime("%H:%M:%S")+"\t"+proceso+": entrada("+entrada+"), salida("+salida+")\n")
+        bitacora.close()
+    else:
+        crearBitacora()
+        añadirBitacora(proceso,entrada,salida)
+    return ""
+
+def crearBitacora():
+    """
+    Funcionamiento: Crea el archivo de texto bitácora.
+    Entradas: 
+    -NA
+    Salidas:
+    -Un arcivho tipo txt llamado bitácora.
+    """       
+    bitacora = open("Bitácora.txt","w")
+    bitacora.close()
+    return"" 
--- main.py
+++ main.py
@@ -4,20 +4,28 @@
 #Versión: 3.8.2
 from funciones import *
 from validaciones import *
-#Declaración de funciones
+#----------------------------------------------------------------------#
+#                           Entrada/Salida                             #
+#----------------------------------------------------------------------#
 
+#---CÓDIGO murcielago
 def cifrarMurcielagoES():
     """
     Funcionamiento: pide al usuario el texto a cifrar
     Entradas:
     Salidas: retorna la impresión al usuario del texto cifrado
     """
-    texto = input("Ingrese el texto a cifrar: ")
-    if validarLetras(texto) and validarVacio(texto):
-        return print("Su texto cifrado en código murciélago es: "+cifrarMurcielago(texto))
+    texto = input("Ingrese la palabra que desea cifrar: ")
+    if not validarVacio(texto):
+        print("Debe ingresar un texto")
+    elif  validarEspacios(texto):
+        print("La cadena no contiene un mensaje a cifrar.")
+    elif not validarLetras(texto):
+        print("No deben de haber caracteres especiales, ni números.")
     else:
-        return print("Entrada inválida, no ingrese caracteres especiales ni números")
-    
+        print("El código cifrado es: "+cifrarMurcielago(texto))
+        añadirBitacora("Murcielago-Cifrar",texto,cifrarMurcielago(texto))
+    return False
 def descifrarMurcielagoES():
     """
     Funcionamiento: pide al usuario el texto a descifrar
@@ -25,23 +33,34 @@ def descifrarMurcielagoES():
     Salidas: retorna la impresión al usuario del texto descifrado
     """
     texto = input("Ingrese el texto a descifrar: ")
-    if validarMurcielago(texto) and validarVacio(texto):
-        return print("Su texto descifrado del código murciélago es: "+descifrarMurcielago(texto))
+    if not validarVacio(texto):
+        print("Debe ingresar un texto")
+    elif  validarEspacios(texto):
+        print("La cadena no contiene un mensaje a descifrar.")
+    elif not validarMurcielago(texto):
+        print("Tiene un caracter inválido.")
     else:
-        return print("El código que ingresó no es válido, intente con el formato h967*70489")
-
+        print("El código descifrado es: "+descifrarMurcielago(texto))
+        añadirBitacora("Murcielago-Descifrar",texto,descifrarMurcielago(texto))
+    return False
+#---CÓDIGO Cenit polar
 def cifrarCenitPolarES():
     """
-    Funcionamiento: pide al usuario el texto a descifrar
+    Funcionamiento: pide al usuario el texto a cifrar
     Entradas:
-    Salidas: retorna la impresión al usuario del texto descifrado
+    Salidas: retorna la impresión al usuario del texto cifrado
     """
-    texto = input("Ingrese el texto a cifrar: ")
-    if validarLetras(texto) and validarVacio(texto):
-        return print("Su texto cifrado en código cenit polar es: "+cifrarCenitPolar(texto))
+    texto = input("Ingrese la palabra que desea cifrar: ")
+    if not validarVacio(texto):
+        print("Debe ingresar un texto")
+    elif  validarEspacios(texto):
+        print("La cadena no contiene un mensaje a cifrar.")
+    elif not validarLetras(texto):
+        print("No deben de haber caracteres especiales, ni números.")
     else:
-        return print("Entrada inválida, no ingrese caracteres especiales ni números")
-
+        print("El código cifrado es: "+cifrarCenitPolar(texto))
+        añadirBitacora("CenitPolar-Cifrar",texto,cifrarCenitPolar(texto))
+    return False
 def descifrarCenitPolarES():
     """
     Funcionamiento: pide al usuario el texto a descifrar
@@ -49,23 +68,31 @@ def descifrarCenitPolarES():
     Salidas: retorna la impresión al usuario del texto descifrado
     """
     texto = input("Ingrese el texto a descifrar: ")
-    if validarCenit(texto) and validarVacio(texto):
-        return print("Su texto descifrado del cenit polar es: "+cifrarCenitPolar(texto))
+    if not validarVacio(texto):
+        print("Debe ingresar un texto")
+    elif  validarEspacios(texto):
+        print("La cadena no contiene un mensaje a descifrar.")
+    elif not validarCenit(texto):
+        print("Tiene un caracter inválido.")
     else:
-        return print("El código que ingresó no es válido, intente con el formato heni¬image")
-
+        print("El código descifrado es: "+cifrarCenitPolar(texto))
+        añadirBitacora("CenitPolar-Descifrar",texto,cifrarCenitPolar(texto))
+    return False
+#---CÓDIGO Deletreo
 def cifrarDeletreoES():
     """
-    Funcionamiento: pide al usuario el texto a descifrar
+    Funcionamiento: pide al usuario el texto a cifrar
     Entradas:
-    Salidas: retorna la impresión al usuario del texto descifrado
+    Salidas: retorna la impresión al usuario del texto cifrado
     """
-    texto = input("Ingrese el texto a cifrar: ")
-    if validarDeletreoCifrar(texto) and validarVacio(texto):
-        return print("Su texto cifrado en código deletreo es: "+cifrarDeletreo(texto))
+    texto = input("Ingrese la palabra que desea cifrar: ")
+    if not validarVacio(texto):
+        print("Debe ingresar un texto")
+    elif not validarDeletreoCifrar(texto):
+        print("Solo puede ser una palabra, únicamente letras")
     else:
-        return print("Entrada inválida, no ingrese caracteres especiales ni números")
-
+        print("El código cifrado es: "+cifrarDeletreo(texto))
+        añadirBitacora("Deletreo-Cifrar",texto,descifrarDeletreo(texto))
 def descifrarDeletreoES():
     """
     Funcionamiento: pide al usuario el texto a descifrar
@@ -73,11 +100,137 @@ def descifrarDeletreoES():
     Salidas: retorna la impresión al usuario del texto descifrado
     """
     texto = input("Ingrese el texto a descifrar: ")
-    if validarDeletreoDescifrar(texto) and validarVacio(texto):
-        return print("Su texto descifrado del código murciélago es: "+descifrarDeletreo(texto))
+    if not validarVacio(texto):
+        print("Debe ingresar un texto")
+    elif  validarEspacios(texto):
+        print("La cadena no contiene un mensaje a descifrar.")
+    elif not validarDeletreoDescifrar(texto):
+        print("Tiene un caracter inválido.")
     else:
-        return print("El código que ingresó no es válido, intente con el formato Oscar~Lima~Alfa")
-
+        print("El código descifrado es: "+descifrarDeletreo(texto))
+        añadirBitacora("Deletreo-Descifrar",texto,descifrarDeletreo(texto))
+    return False
+#---CÓDIGO EUCALIPTO
+def cifrarEucaliptoES():
+    """
+    Funcionamiento: Se encarga de la entrada, salida y validación de datos, además de mostrar un mensaje si ocurre un error
+    Entradas:
+    -NA
+    Salidas: 
+    -Mensajes informativos si ocurre errores
+    -El código encriptado
+    """
+    texto = input("Ingrese la palabra que desea cifrar: ")
+    if not validarVacio(texto):
+        print("Debe ingresar un texto")
+    elif  validarEspacios(texto):
+        print("La cadena no contiene un mensaje a cifrar.")
+    elif not validarLetras(texto):
+        print("No deben de haber caracteres especiales, ni números.")
+    else:
+        print("El código cifrado es: "+cifrarEucalipto(texto))
+        añadirBitacora("Eucalipto-Cifrar",texto,cifrarEucalipto(texto))
+    return False
+def descifrarEucaliptoES():
+    """
+    Funcionamiento: Se encarga de la entrada, salida y validación de datos, además de mostrar un mensaje si ocurre un error
+    Entradas:
+    -NA
+    Salidas: 
+    -Mensajes informativos si ocurre errores
+    -El código encriptado
+    """
+    texto = input("Ingrese la palabra que se desea decifrar: ")
+    if not validarVacio(texto):
+        print("Debe ingresar un texto")
+    elif  validarEspacios(texto):
+        print("La cadena no contiene un mensaje a descifrar.")
+    elif not validarEucaliptoDescifrar(texto):
+        print("Tiene un caracter inválido.")
+    else:
+        print("El código descifrado es: "+descifrarEucalipto(texto))
+        añadirBitacora("Eucalipto-Descifrar",texto,descifrarEucalipto(texto))
+    return False
+#---CÓDIGO MORSE
+def cifrarMorseES():
+    """
+    Funcionamiento: Se encarga de la entrada, salida y validación de datos, además de mostrar un mensaje si ocurre un error
+    Entradas:
+    -NA
+    Salidas: 
+    -Mensajes informativos si ocurre errores
+    -El código encriptado
+    """
+    texto = input("Ingrese la palabra que desea cifrar: ")
+    if not validarVacio(texto):
+        print("Debe ingresar un texto")
+    elif  validarEspacios(texto):
+        print("La cadena no contiene un mensaje a cifrar.")
+    elif not validarMorseCifrar(texto):
+        print("No deben de haber caracteres especiales o usa un caracter que no existe en morse.")
+    else:
+        print("El código cifrado es: "+cifrarMorse(texto))
+        añadirBitacora("Morse-Cifrar",texto,cifrarMorse(texto))
+    return False
+def descifrarMorseES():
+    """
+    Funcionamiento: Se encarga de la entrada, salida y validación de datos, además de mostrar un mensaje si ocurre un error
+    Entradas:
+    -NA
+    Salidas: 
+    -Mensajes informativos si ocurre errores
+    -El código encriptado
+    """
+    texto = input("Ingrese la palabra que desea descifrar: ")
+    if not validarVacio(texto):
+        print("Debe ingresar un texto")
+    elif  validarEspacios(texto):
+        print("La cadena no contiene un mensaje a descifrar.")
+    elif not validarMorseDescifrar(texto):
+        print("No sigue el formato de morse, los caracteres válidos son: '.-^|'")        
+    else:
+        print("El código descifrado es: "+descifrarMorse(texto))
+        añadirBitacora("Morse-Descifrar",texto,descifrarMorse(texto))
+    return False
+#SUFAMELICO
+def cifrarSufamelicoES():
+    """
+    Funcionamiento: Se encarga de la entrada, salida y validación de datos, además de mostrar un mensaje si ocurre un error
+    Entradas:
+    -NA
+    Salidas: 
+    -Mensajes informativos si ocurre errores
+    -El código cifrado
+    """
+    texto = input("Ingrese la palabra que desea cifrar: ")
+    if not validarVacio(texto):
+        print("Debe ingresar un texto")
+    elif  validarEspacios(texto):
+        print("La cadena no contiene un mensaje a cifrar.")
+    elif not validarLetras(texto):
+        print("No deben de haber caracteres especiales, ni números.")
+    else:
+        print("El código descifrado es: "+codigoSufamelico(texto))
+        añadirBitacora("Sufamélico-Cifrar",texto,codigoSufamelico(texto))
+    return False
+def descifrarSufamelicoES():
+    """
+    Funcionamiento: Se encarga de la entrada, salida y validación de datos, además de mostrar un mensaje si ocurre un error
+    Entradas:
+    -NA
+    Salidas: 
+    -Mensajes informativos si ocurre errores
+    -El código descifrado
+    """
+    texto = input("Ingrese la palabra que se desea descifrar: ")
+    if not validarVacio(texto):
+        print("Debe ingresar un texto")
+    elif  validarEspacios(texto):
+        print("La cadena no contiene un mensaje a descifrar.")
+    else:
+        print("El código cifrado es: "+codigoSufamelico(texto))
+        añadirBitacora("Sufamélico-Descifrar",texto,codigoSufamelico(texto))
+    return False
 
 #cifrarMurcielagoES()
 #descifrarMurcielagoES()
@@ -85,3 +238,67 @@ def descifrarDeletreoES():
 #descifrarCenitPolarES()
 #cifrarDeletreoES()
 #descifrarDeletreoES()
+
+def menu():
+    print("#---------------------------------------------------------------#")
+    print("  Convertidor de español a códigos, claves y señales de pista")
+    print("#---------------------------------------------------------------#")
+    print("1.Clave Murciélago")
+    print("2.Clave Eucalipto")
+    print("3.Clave Cenit Polar")
+    print("4.Código Morse")
+    print("5.Clave Sufamélico")
+    print("6.Clave Deletreo")
+    print("7.Salir\n")
+    opcion1 = input("Seleccione la opción que desee: ")
+    if validarEntero(opcion1):
+        opcion1 = int(opcion1)
+        if opcion1 == 7:
+            return ""
+        elif opcion1 <= 6 and opcion1 > 0:
+            opcion2 = input("Desea\n1.Cifrar\n2.Descifrar\n")
+            if validarEntero(opcion2):
+                opcion2 = int(opcion2)
+                if opcion2 <= 2 and opcion2 > 0:
+                    if opcion1 == 1:
+                        if opcion2 == 1:
+                            cifrarMurcielagoES()
+                        else:
+                            descifrarMurcielagoES()
+                    elif opcion1 == 2:
+                        if opcion2 == 1:
+                            cifrarEucaliptoES()
+                        else:
+                            descifrarEucaliptoES()
+                    elif opcion1 == 3:
+                        if opcion2 == 1:
+                            cifrarCenitPolarES()
+                        else:
+                            descifrarCenitPolarES()           
+                    elif opcion1 == 4:
+                        if opcion2 == 1:
+                            cifrarMorseES()
+                        else:
+                            descifrarMorseES()           
+                    elif opcion1 == 5:
+                        if opcion2 == 1:
+                            cifrarSufamelicoES()
+                        else:
+                            descifrarSufamelicoES()
+                    else:
+                        if opcion2 == 1:
+                            cifrarDeletreoES()
+                        else:
+                            descifrarDeletreoES()
+                else:
+                    print("Opción fuera de rango")
+            else:
+                print("Entrada inválida, ingrese el número de una de las opciones")        
+        else:
+            print("Opción fuera de rango") 
+    else:
+        print("Entrada inválida, ingrese el número de una de las opciones")
+    input("")
+    menu()
+        
+menu()
